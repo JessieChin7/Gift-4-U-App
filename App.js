e909456ebd985d0f4afa7986639c58c1b163f2c9
JessieChin7 <yuan2001@live.com>
2023-04-14 10:53:01 +0800

    feat(0414): test checkbox

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { CheckBox } from 'react-native-elements';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { styles } from './App-styles';
 export default function App() {
+  const [checkedItems, setCheckedItems] = useState([]);
+
+  const items = [
+    { id: 1, title: '價格命運抽抽抽' },
+    { id: 2, title: '收禮方式抽抽抽' },
+    { id: 3, title: '包裝方式抽抽抽' },
+    { id: 4, title: '送禮者線索數量' },
+  ];
+
+  const toggleChecked = (id) => {
+    if (checkedItems.includes(id)) {
+      setCheckedItems(checkedItems.filter((item) => item !== id));
+    } else {
+      setCheckedItems([...checkedItems, id]);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Yeah~~~!</Text>
+      <ScrollView>
+        {items.map((item) => (
+          <CheckBox
+            key={item.id}
+            title={item.title}
+            checked={checkedItems.includes(item.id)}
+            onPress={() => toggleChecked(item.id)}
+          />
+        ))}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
